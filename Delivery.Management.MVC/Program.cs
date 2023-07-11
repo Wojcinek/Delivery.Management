8576fade5aee57b0cd54161985298713e24768f5
wojci <wojci@DESKTOP-PK2VBRC>
2023-07-11 20:36:27 +0200

    Swagger

--- Delivery.Management.MVC/Program.cs
+++ Delivery.Management.MVC/Program.cs
@@ -1,6 +1,20 @@
+using Delivery.Management.MVC.Services;
+using System.Reflection;
+using Delivery.Management.MVC.Services.Base;
+using Delivery.Management.MVC.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddHttpClient<IClient, Client>(cl => cl.BaseAddress = new Uri("https://localhost:44309"));
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+//builder.Services.AddScoped<IDelvieryAllocationController, DeliveryAllocationService>();
+
+builder.Services.AddSingleton<ILocalStorageService, LocalStorageService>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -12,6 +26,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
